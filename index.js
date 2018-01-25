4f7d64fda76aa494ad16c41d74ec69e343fcf1f1
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-25 04:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine,name) {
+<<<<<<< HEAD
    katzDeliLine.push(name)
    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
@@ -23,3 +24,17 @@ function currentLine(katzDeliLine) {
   }
   return txt.slice(0,-1);
 }
+=======
+  return katzDeliLine.push(name);
+
+}
+
+function nowServing(){
+  katzDeli.pop();
+  return "There is nobody waiting to be served!";
+}
+
+function currentLine() {
+  v
+}
+>>>>>>> eebb51f9b4446f4a042c2b453bb687d509fcc4e6
